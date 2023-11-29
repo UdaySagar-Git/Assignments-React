c47814f1482e0899ce8dcd2532a1ab9ba73d6409
Uday Sagar <udaysagar.mail@gmail.com>
2023-11-29 12:28:35 +0530

    modified App.jsx and some img links

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,6 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
   const { pathname } = useLocation();
-  return (
-    <div className="">
-      <div className="flex justify-center items-center bg-slate-600 gap-20  text-white h-[30px] sticky top-0">
-        <h1 className="text-xl font-bold ">You are currently in : {pathname}</h1>
-        <Link to="/"><button className="bg-rose-600 text-white px-4 py-1 rounded-md">Home</button></Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/assignment-1" element={<Assignment1 />} />
-        <Route path="/assignment-2" element={<Assignment2 />} />
-        <Route path="/assignment-3" element={<Assignment3 />} />
-        <Route path="/assignment-4-1" element={<Assignment4_1 />} />
-        <Route path="/assignment-4-2" element={<Assignment4_2 />} />
-        <Route path="/assignment-5" element={<Assignment5 />} />
-        <Route path="/assignment-6" element={<Assignment6 />} />
-        <Route path="/assignment-7-1" element={<Assignment7_1 />} />
-        <Route path="/assignment-7-2" element={<Assignment7_2 />} />
-        <Route path="/assignment-8" element={<Assignment8 />} />
-        <Route path="/assignment-9/*" element={<Assignment9 />} />
-      </Routes>
-
-    </div>
-  );
-}
-export default App;
-
-import { Link } from 'react-router-dom';
-
-const Home = () => {
-
   const assignmentsArray = [
     {
       name: "Assignment-1",
@@ -105,14 +75,45 @@ const Home = () => {
   ]
 
   return (
+    <div className="">
+      <div className="flex justify-center items-center bg-slate-600 gap-20 text-white h-[35px] sticky top-0">
+        <h1 className="text-xl font-bold">You are currently in :<span className='font-mono text-red-500 bg-black px-2 py-1 mx-3 rounded-md'>{pathname}</span></h1>
+        <Link to="/"><button className="bg-rose-600 text-white px-4 py-1 rounded-md">Home</button></Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home assignmentsArray={assignmentsArray}/>} />
+        <Route path="/assignment-1" element={<Assignment1 />} />
+        <Route path="/assignment-2" element={<Assignment2 />} />
+        <Route path="/assignment-3" element={<Assignment3 />} />
+        <Route path="/assignment-4-1" element={<Assignment4_1 />} />
+        <Route path="/assignment-4-2" element={<Assignment4_2 />} />
+        <Route path="/assignment-5" element={<Assignment5 />} />
+        <Route path="/assignment-6" element={<Assignment6 />} />
+        <Route path="/assignment-7-1" element={<Assignment7_1 />} />
+        <Route path="/assignment-7-2" element={<Assignment7_2 />} />
+        <Route path="/assignment-8" element={<Assignment8 />} />
+        <Route path="/assignment-9/*" element={<Assignment9 />} />
+      </Routes>
+
+    </div>
+  );
+}
+export default App;
+
+import { Link } from 'react-router-dom';
+
+const Home = ({assignmentsArray}) => {
+
+  return (
     <div className="min-h-screen flex justify-center items-center bg-slate-700  homecss">
       <div className="flex flex-col gap-2 justify-center ">
         <h1 className="text-4xl font-bold text-center text-white mb-5">React Assignments</h1>
         {assignmentsArray.map((assignment, index) => (
-            <div key={index}>
-              <Link to={assignment.link}><button>{assignment.name}</button></Link>
-              <a href={assignment.code} target="_blank" ><button>Code</button></a>
-            </div>
+          <div key={index}>
+            <Link to={assignment.link}><button>{assignment.name}</button></Link>
+            <a href={assignment.code} target="_blank" ><button>Code</button></a>
+          </div>
         ))}
       </div>
     </div>
